0c3ed0f871c4f7a7bfc03cf78b690831bb2896e7
atsushimemet <noap3b69n@gmail.com>
2023-04-29 23:48:43 +0900

    chore: 不要なシークレットなどは削除

--- vook_db_v4/config.py
+++ vook_db_v4/config.py
@@ -1,3 +1,3 @@
-from local_config import AppId, ClientId, ClientSecret, pid, sid
+from vook_db_v4.local_config import ClientId, pid, sid
 
 aff_id = f"//ck.jp.ap.valuecommerce.com/servlet/referral?vs={sid}&vp={pid}&vc_url="
