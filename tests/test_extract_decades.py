7358c4c2297b6a06e3ed85f31e54e136c2e4022f
atsushimemet <noap3b69n@gmail.com>
2023-04-30 21:48:33 +0900

    test: test_extract_decades_both.pyの追加
    test: extract_decades_both_pat_1の正常系 - マッチ
    
    test: extract_decades_both_pat_1の正常系 - not マッチ
    
    test: extract_decades_both_pat_1の異常系 - 開始年の繰り返し
    
    test: extract_decades_both_pat_1の異常系 - 終了年の繰り返し
    
    refactor: TestExtractDecadesBothPat1の生成
    
    refactor: item_name_match, item_name_not_matchフィクスチャの追加
    
    chore: testsディレクトリにextract_decadesを作成
    
    chore: test_extract_decades.pyのリネーム
    
    test: extract_decades_both_pat_2
    
    chore: 不要なテストの削除
    
    test: extract_decades_both_pat_3
    
    refactor: test_extract_decades.py- both pat_1~pat3
    
    refactor: test_extract_decades.py- both pat_4
    
    refactor: test_extract_decades.py- both pat_5
    
    refactor: test_extract_decades.py- both pat_6

---
+++ tests/test_extract_decades.py
@@ -0,0 +1,60 @@
+import pytest
+
+from vook_db_v4.extract_decades import (
+    extract_decades_both_pat_1,
+    extract_decades_both_pat_2,
+    extract_decades_both_pat_3,
+    extract_decades_both_pat_4,
+    extract_decades_both_pat_5,
+    extract_decades_both_pat_6,
+)
+
+item_name_match = [
+    "Levi's リーバイス 1950~60年代 Levi's Vintage Big Poster ヴィンテージビッグポスター 広告用 ビンテージ ビッグサイズ",
+    "Levi's リーバイス 50's 60's Levi's Vintage Big Poster ヴィンテージビッグポスター 広告用 ビンテージ ビッグサイズ",
+    "Levi's リーバイス 50~60年代 Levi's Vintage Big Poster ヴィンテージビッグポスター 広告用 ビンテージ ビッグサイズ",
+    "Levi's リーバイス 50s-60s Levi's Vintage Big Poster ヴィンテージビッグポスター 広告用 ビンテージ ビッグサイズ",
+    "Levi's リーバイス 1950-1960 Levi's Vintage Big Poster ヴィンテージビッグポスター 広告用 ビンテージ ビッグサイズ",
+    "Levi's,リーバイス,1950~60年代,Levi's,Vintage,Big,Poster,ヴィンテージビッグポスター,広告用,ビンテージ,ビッグサイズ",
+]
+
+item_name_not_match = [
+    "Levi's リーバイス 1950 Levi's Vintage Big Poster ヴィンテージビッグポスター 広告用 ビンテージ ビッグサイズ"
+    for _ in range(len(item_name_match))
+]
+
+extract_decades_func_list = [
+    extract_decades_both_pat_1,
+    extract_decades_both_pat_2,
+    extract_decades_both_pat_3,
+    extract_decades_both_pat_4,
+    extract_decades_both_pat_5,
+    extract_decades_both_pat_6,
+]
+
+parametrize_args_match = [
+    (v1, v2) for v1, v2 in zip(item_name_match, extract_decades_func_list)
+]
+parametrize_args_not_match = [
+    (v1, v2) for v1, v2 in zip(item_name_not_match, extract_decades_func_list)
+]
+
+
+class TestExtractDecades:
+    @pytest.mark.parametrize(
+        "item_name,func",
+        parametrize_args_match,
+    )
+    def test_valid_match(self, item_name, func):
+        actual = func(item_name)
+        expected = ("1950", "1960")
+        assert actual == expected
+
+    @pytest.mark.parametrize(
+        "item_name,func",
+        parametrize_args_not_match,
+    )
+    def test_valid_not_match(self, item_name, func):
+        actual = func(item_name)
+        expected = None
+        assert actual == expected
