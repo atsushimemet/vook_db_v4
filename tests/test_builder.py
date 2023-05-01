0ad0164e86f09f0f4b1e2a5571d2185247efd933
atsushimemet <noap3b69n@gmail.com>
2023-05-01 18:28:34 +0900

    年代情報を取得するモジュール等の命名にdecadesを使用しているが、ageに修正 (#32)
    * chore: ファイル名に含まれるdecadesをageへ変更
    
    * chore: 変数名に含まれるdecadesをageへ変更

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 from vook_db_v4.builder import AgeBuilder, ItemBuilder
-from vook_db_v4.extract_decades import extract_decades_func_list
+from vook_db_v4.extract_age import extract_age_func_list
 from vook_db_v4.extract_item import extract_item_func_list
 from vook_db_v4.validates import validate_19XX_fullmatch, validate_item_fullmatch
 
@@ -13,7 +13,7 @@ def test_run_age_builder_extract():
     file_name = "20230430_132941_for_issue_11.csv"
     item_name = pd.read_csv(OUTPUT_PATH / file_name)["itemName"]
     actual = AgeBuilder(
-        item_name, extract_decades_func_list, validate_19XX_fullmatch
+        item_name, extract_age_func_list, validate_19XX_fullmatch
     ).extract()
     assert actual
 
