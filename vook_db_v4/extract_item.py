d115ac7b8e54d538605e7015b0ecd036a7bdf101
atsushimemet <noap3b69n@gmail.com>
2023-05-01 16:56:56 +0900

    feat: extract_item_denim_jacket関数の追加

--- vook_db_v4/extract_item.py
+++ vook_db_v4/extract_item.py
@@ -7,3 +7,11 @@ def extract_item_denim(item_name: str):
         return "denim"
     else:
         return None
+
+
+def extract_item_denim_jacket(item_name: str):
+    match = re.search(r"ジージャン|Ｇジャン|デニムジャケット|デニム ジャケット", item_name)
+    if match:
+        return "denim jacket"
+    else:
+        return None
