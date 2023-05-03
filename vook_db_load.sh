fcd4090e0fc6e02adc67361319abc4c03975168b
atsushimemet <noap3b69n@gmail.com>
2023-05-03 23:23:13 +0900

    fix: cronが実行できるようにホームディレクトリ表記を変更

--- vook_db_load.sh
+++ vook_db_load.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # データの前処理
-# python ./vook_db_v4/get_yahoo_data_v2.py
+# python /home/ec2-user/vook_db_v4/get_yahoo_data_v2.py
 
 # DB設定値を読み込み
-source ./env/database.conf
+source /home/ec2-user/env/database.conf
 
 # ログファイルの名前を設定する
-log_file="./log/vook_db_load_$(date +%Y%m%d%H%M%S).log"
+log_file="/home/ec2-user/log/vook_db_load_$(date +%Y%m%d%H%M%S).log"
 
 # ログファイルに開始時間を書き込む
 echo "Start time: $(date +%Y-%m-%d_%H:%M:%S)" >> "$log_file"
 
 # CSVファイルをMySQLのplatformsテーブルに挿入する
 mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -e "
-  LOAD DATA LOCAL INFILE './data/output/platforms.csv' 
+  LOAD DATA LOCAL INFILE '/home/ec2-user/data/output/platforms.csv' 
   INTO TABLE platforms 
   FIELDS TERMINATED BY ',' 
   ENCLOSED BY '\"' 
@@ -22,7 +22,7 @@ mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -
 "
 # CSVファイルをMySQLのagesテーブルに挿入する
 mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -e "
-  LOAD DATA LOCAL INFILE './data/output/ages.csv' 
+  LOAD DATA LOCAL INFILE '/home/ec2-user/data/output/ages.csv' 
   INTO TABLE ages 
   FIELDS TERMINATED BY ',' 
   ENCLOSED BY '\"' 
@@ -31,7 +31,7 @@ mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -
 "
 # CSVファイルをMySQLのbrandsテーブルに挿入する
 mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -e "
-  LOAD DATA LOCAL INFILE './data/output/brands.csv' 
+  LOAD DATA LOCAL INFILE '/home/ec2-user/data/output/brands.csv' 
   INTO TABLE brands 
   FIELDS TERMINATED BY ',' 
   ENCLOSED BY '\"' 
@@ -40,7 +40,7 @@ mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -
 "
 # CSVファイルをMySQLのitemsテーブルに挿入する
 mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -e "
-  LOAD DATA LOCAL INFILE './data/output/items.csv' 
+  LOAD DATA LOCAL INFILE '/home/ec2-user/data/output/items.csv' 
   INTO TABLE items 
   FIELDS TERMINATED BY ',' 
   ENCLOSED BY '\"' 
@@ -49,7 +49,7 @@ mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -
 "
 # CSVファイルをMySQLのlineテーブルに挿入する
 mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -e "
-  LOAD DATA LOCAL INFILE './data/output/line.csv' 
+  LOAD DATA LOCAL INFILE '/home/ec2-user/data/output/line.csv' 
   INTO TABLE line 
   FIELDS TERMINATED BY ',' 
   ENCLOSED BY '\"' 
@@ -59,7 +59,7 @@ mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -
 
 # CSVファイルをMySQLのproductsテーブルに挿入する
 mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -e "
-  LOAD DATA LOCAL INFILE './data/output/products.csv' 
+  LOAD DATA LOCAL INFILE '/home/ec2-user/data/output/products.csv' 
   INTO TABLE products 
   FIELDS TERMINATED BY ',' 
   ENCLOSED BY '\"' 
