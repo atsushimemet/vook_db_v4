6d888e87e60adebb9f23d93974ecced4e44e2270
atsushimemet <noap3b69n@gmail.com>
2023-05-03 23:32:10 +0900

    feat: ログのアップデート

--- vook_db_load.sh
+++ vook_db_load.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # データの前処理
+# ログファイルの名前を設定する
+log_file="/home/ec2-user/vook_db_v4/log/vook_db_load_$(date +%Y%m%d%H%M%S).log"
+
+# ログファイルにデータ前処理開始時間を書き込む
+echo "Start time - get_yahoo_data_v2.py: $(date +%Y-%m-%d_%H:%M:%S)" >> "$log_file"
 python /home/ec2-user/vook_db_v4/vook_db_v4/get_yahoo_data_v2.py
+# ログファイルにデータ前処理終了時間を書き込む
+echo "End time - get_yahoo_data_v2.py: $(date +%Y-%m-%d_%H:%M:%S)" >> "$log_file"
 
 # DB設定値を読み込み
 source /home/ec2-user/vook_db_v4/env/database.conf
 
-# ログファイルの名前を設定する
-log_file="/home/ec2-user/vook_db_v4/log/vook_db_load_$(date +%Y%m%d%H%M%S).log"
-
-# ログファイルに開始時間を書き込む
-echo "Start time: $(date +%Y-%m-%d_%H:%M:%S)" >> "$log_file"
-
+# ログファイルにload infile開始時間を書き込む
+echo "Start time - load_infile: $(date +%Y-%m-%d_%H:%M:%S)" >> "$log_file"
 # CSVファイルをMySQLのplatformsテーブルに挿入する
 mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -e "
   LOAD DATA LOCAL INFILE '/home/ec2-user/vook_db_v4/data/output/platforms.csv' 
@@ -67,5 +70,5 @@ mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} --local-infile=1 ${DB_NAME} -
   IGNORE 1 ROWS;
 "
 
-# ログファイルに終了時間を書き込む
-echo "End time: $(date +%Y-%m-%d_%H:%M:%S)" >> "$log_file"
+# ログファイルにload infile終了時間を書き込む
+echo "Start time - load_infile: $(date +%Y-%m-%d_%H:%M:%S)" >> "$log_file"
