812165848eea715c04d3d19f055bb77daf55704a
atsushimemet <noap3b69n@gmail.com>
2023-05-03 16:28:18 +0900

    refactor: 設定値の設定ファイル移植

--- vook_db_v4/get_yahoo_data_v2.py
+++ vook_db_v4/get_yahoo_data_v2.py
@@ -1,61 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
-import datetime
 import hashlib
 import json
 
 import pandas as pd
 import requests
 
-from vook_db_v4.config import ClientId, aff_id
+from vook_db_v4.config import (
+    AGES_ID,
+    BRAND,
+    BRAND_ID,
+    INFO_GET_DATE,
+    ITEM,
+    ITEM_ID,
+    LINE,
+    LINE_ID,
+    PLATFORM_ID,
+    REQ_URL_CATE,
+    RUN_TIME,
+    TABLE_COLUMNS,
+    WANT_ITEMS,
+    params,
+)
 
 
 def main():
-    REQ_URL_CATE = "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch"
-
-    WANT_ITEMS = [
-        "itemCode",
-        "itemName",
-        "itemPrice",
-        "itemUrl",
-    ]
-    PLATFORM_ID = 2
-    BRAND = "リーバイス"
-    BRAND_ID = 1
-    ITEM = "デニム"
-    ITEM_ID = 1
-    LINE = "66前期"
-    LINE_ID = 1
-    # START_AGE = 1974
-    # END_AGE = 1977
-    AGES_ID = 1
-    RUN_TIME = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
-    INFO_GET_DATE = datetime.datetime.today()
-    TABLE_COLUMNS = [
-        "product_id",
-        "product_name",
-        "platform_id",
-        "ages_id",
-        "brand_id",
-        "item_id",
-        "line_id",
-        "price",
-        "info_get_date",
-        "status",
-    ]
-
-    query = f"{BRAND} ヴィンテージ {ITEM} {LINE}"
-
-    params = {
-        "appid": ClientId,
-        "output": "json",
-        "query": query,
-        "sort": "-price",
-        "affiliate_id": aff_id,
-        "affiliate_type": "vc",
-        "results": 100,  # NOTE: 100個ずつしか取得できない。
-    }
-
     start_num = 1
     step = 100
     max_products = 1000
