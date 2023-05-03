bb5e8c8b3623dac89d43af516f6066e6d51dc6b5
atsushimemet <noap3b69n@gmail.com>
2023-05-03 22:11:17 +0900

    fix: 関連テーブルの作成

--- vook_db_load.sh
+++ vook_db_load.sh
@@ -5,6 +5,52 @@
 # DB設定値を読み込み
 source ./env/database.conf
 
+# CSVファイルをMySQLのplatformsテーブルに挿入する
+mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} -e "
+  LOAD DATA LOCAL INFILE './data/output/platforms.csv' 
+  INTO TABLE products 
+  FIELDS TERMINATED BY ',' 
+  ENCLOSED BY '\"' 
+  LINES TERMINATED BY '\n' 
+  IGNORE 1 ROWS;
+"
+# CSVファイルをMySQLのagesテーブルに挿入する
+mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} -e "
+  LOAD DATA LOCAL INFILE './data/output/ages.csv' 
+  INTO TABLE products 
+  FIELDS TERMINATED BY ',' 
+  ENCLOSED BY '\"' 
+  LINES TERMINATED BY '\n' 
+  IGNORE 1 ROWS;
+"
+# CSVファイルをMySQLのbrandsテーブルに挿入する
+mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} -e "
+  LOAD DATA LOCAL INFILE './data/output/brands.csv' 
+  INTO TABLE products 
+  FIELDS TERMINATED BY ',' 
+  ENCLOSED BY '\"' 
+  LINES TERMINATED BY '\n' 
+  IGNORE 1 ROWS;
+"
+# CSVファイルをMySQLのitemsテーブルに挿入する
+mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} -e "
+  LOAD DATA LOCAL INFILE './data/output/items.csv' 
+  INTO TABLE products 
+  FIELDS TERMINATED BY ',' 
+  ENCLOSED BY '\"' 
+  LINES TERMINATED BY '\n' 
+  IGNORE 1 ROWS;
+"
+# CSVファイルをMySQLのlineテーブルに挿入する
+mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} -e "
+  LOAD DATA LOCAL INFILE './data/output/line.csv' 
+  INTO TABLE products 
+  FIELDS TERMINATED BY ',' 
+  ENCLOSED BY '\"' 
+  LINES TERMINATED BY '\n' 
+  IGNORE 1 ROWS;
+"
+
 # CSVファイルをMySQLのproductsテーブルに挿入する
 mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} -e "
   LOAD DATA LOCAL INFILE './data/output/products.csv' 
